5b140af1c95445d13dad839efce1b8e475ef1986
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2019-06-18 19:44:13 +0530

    Update __init__.py

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,3 +1,19 @@
+os.chdir('..')
+os.chdir('..')
+os.chdir('..')
+os.chdir('..')
+os.chdir('content')
+os.chdir('caffe')
+os.chdir('python')
+os.chdir('caffe')
+
+#sys.path.append('/content/caffe/python/caffe')
+#sys.path.append('/content/caffe/src/caffe/solvers')
+#sys.path.append('/content/caffe/python/caffe/test')
+print("this is sys path ",sys.path)
+print("this is current working directory")
+print(os.getcwd())
+
 from .pycaffe import Net, SGDSolver, NesterovSolver, AdaGradSolver, RMSPropSolver, AdaDeltaSolver, AdamSolver, NCCL, Timer
 from ._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
 from ._caffe import __version__
