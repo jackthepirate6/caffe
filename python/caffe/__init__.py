1180ae273e2264084fd968f8c1eb3ae53c426efb
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2019-06-19 10:59:31 +0530

    Update __init__.py

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,18 +1,21 @@
 import os
 import sys
-sys.path.append('/content/caffe/python/caffe/')
-sys.path.append('/content/caffe/python/caffe')
 
-
-os.chdir('caffe')
-os.chdir('python')
-os.chdir('caffe')
-print(os.getcwd())
-#from .pycaffe import Net, SGDSolver, NesterovSolver, AdaGradSolver, RMSPropSolver, AdaDeltaSolver, AdamSolver, NCCL, Timer
-from ._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
-from ._caffe import __version__
-from .proto.caffe_pb2 import TRAIN, TEST
-from .classifier import Classifier
-from .detector import Detector
-from . import io
-from .net_spec import layers, params, NetSpec, to_proto
+try:
+#    from .pycaffe import Net, SGDSolver, NesterovSolver, AdaGradSolver, RMSPropSolver, AdaDeltaSolver, AdamSolver, NCCL, Timer
+    from ._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
+    from ._caffe import __version__
+    from .proto.caffe_pb2 import TRAIN, TEST
+    from .classifier import Classifier
+    from .detector import Detector
+    from . import io
+    from .net_spec import layers, params, NetSpec, to_proto
+except ImportError:
+    print("relative import failed")
+    
+try:
+    os.chdir('/')
+    from content.caffe.python.caffe._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
+    from content.caffe.python.caffe._caffe import __version__
+except ImportError:
+    print("absolute import failed")
