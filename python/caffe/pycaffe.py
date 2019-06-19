cb5d092a9c2bcdb60ef11cd9274cceab3e871498
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2019-06-19 16:25:39 +0530

    Update pycaffe.py

--- python/caffe/pycaffe.py
+++ python/caffe/pycaffe.py
@@ -2,7 +2,7 @@
 Wrap the internal caffe C++ module (_caffe.so) with a clean, Pythonic
 interface.
 """
-
+import os
 from collections import OrderedDict
 try:
     from itertools import izip_longest
@@ -10,6 +10,12 @@ except:
     from itertools import zip_longest as izip_longest
 import numpy as np
 
+os.getcwd()
+os.chdir('/')
+os.chdir('content')
+os.chdir('caffe')
+os.chdir('python')
+os.chdir('caffe')
 from ._caffe import Net, SGDSolver, NesterovSolver, AdaGradSolver, \
         RMSPropSolver, AdaDeltaSolver, AdamSolver
 import caffe.io
