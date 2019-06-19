4c463777fe5ff330deb4a0bcd25b0e74bbce886b
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2019-06-19 11:40:53 +0530

    Update __init__.py

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -10,13 +10,37 @@ try:
     from .detector import Detector
     from . import io
     from .net_spec import layers, params, NetSpec, to_proto
-except ImportError:
+except :
     print("relative import failed")
+
+try:
+    os.chdir('/')
+    os.chdir('content')
+    os.chdir('caffe')
+    os.chdir('python')
+    os.chdir('caffe')
+    from ._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
+    from ._caffe import __version__
+    from .proto.caffe_pb2 import TRAIN, TEST
+    from .classifier import Classifier
+    from .detector import Detector
+    from . import io
+    from .net_spec import layers, params, NetSpec, to_proto
+except:
+    print("relative error failed second time")
+    
     
 try:
     os.chdir('/')
     from content.caffe.python.caffe._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
     from content.caffe.python.caffe._caffe import __version__
     print("import successful")
-except ImportError:
+except :
     print("absolute import failed")
+
+try:
+    os.chdir('/')
+    from _caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
+    from _caffe import __version__
+except :
+    print("absolute import failed second time")
