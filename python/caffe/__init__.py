bd60643bffc2bf3b67d50bb8232dfd0ef77c1373
Ajinkya Narnaware <narnawareajinkya@gmail.com>
2019-06-19 13:00:17 +0530

    Update __init__.py

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -33,7 +33,7 @@ try:
     from .detector import Detector
     from . import io
     from .net_spec import layers, params, NetSpec, to_proto
-except:
+except Exception as e:
     print(e)
     print("relative error failed second time")
     
@@ -44,7 +44,8 @@ try:
     from content.caffe.python.caffe._caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
     from content.caffe.python.caffe._caffe import __version__
     print("import successful")
-except :
+except Exception as e:
+    print(e)
     print("absolute import failed")
 
 try:
@@ -52,5 +53,6 @@ try:
 #    from content.caffe.python.caffe.pycaffe import Net, SGDSolver, NesterovSolver, AdaGradSolver, RMSPropSolver, AdaDeltaSolver, AdamSolver, NCCL, Timer    
     from _caffe import init_log, log, set_mode_cpu, set_mode_gpu, set_device, Layer, get_solver, layer_type_list, set_random_seed, solver_count, set_solver_count, solver_rank, set_solver_rank, set_multiprocess, has_nccl
     from _caffe import __version__
-except :
+except Exception as e:
+    print(e)
     print("absolute import failed second time")
